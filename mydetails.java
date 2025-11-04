93828bee0ef02ba7d84dee6d207802b0ebfb9fef
Nabanita <mnabanita246@gmail.com>
2025-11-04 09:53:50 +0530

    Day 1 - HelloWorld program

---
+++ mydetails.java
@@ -0,0 +1,14 @@
+public class mydetails
+{
+    public static void main (String[] args)
+    {
+        String name = "Nabanita";
+        int age = 25;
+        String city = "Kolkata";
+
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("City:" + name);
+
+    }
+}
---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[] args)
+    {
+        int n = 5; 
+        int m=10 ;
+        int s = n+m;
+        System.out.print("The Sum is:" );
+         System.out.println("\t"+s);
+    }
+    
+}
